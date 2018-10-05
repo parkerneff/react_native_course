3bc66f3e66fd819523f9bd8d553f206c58f6c471
pneff <pneff@unicon.net>
2018-10-05 10:21:05 -0700

    45 redux reducers def

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,6 @@
+import {} from '../actions/actionTypes';
+import {ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE} from "../actions/actionTypes";
+
 const initialState = {
     places: [],
     selectedPlace: null
@@ -5,6 +8,45 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat(
+                    {
+                        key: String(Math.random()),
+                        name: action.placeName,
+                        image: {
+                            uri: "https://www.rd.com/wp-content/uploads/2018/04/lanai-400x400.jpg"
+                        }
+                    })
+
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+
+            };
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(
+                    place => {
+                        return place.key === action.placeKey;
+                    }
+                )
+
+            };
+        case DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+
+            };
+
         default:
             return state;
 
